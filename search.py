efb469872b0b5a960633fb834c4661afd7b74b05
Matthew Peddie <mpeddie@gmail.com>
2019-09-02 16:39:42 +1000

    Initial commit: scans.
    To do:
    
     - proper user arguments
     - proper time zone handling based on the lat and lon given
     - star conjunction analysis
     - magnitude difference thresholding

---
+++ search.py
@@ -0,0 +1,318 @@
+import numpy as np
+import ephem  # for magnitude calculations
+from datetime import datetime, timedelta
+from skyfield.api import Star, load, Topos, utc
+from skyfield.data import hipparcos
+from skyfield.units import Angle
+from skyfield import almanac
+
+
+def datetime_range(start, end, delta):
+    current = start
+    while current < end:
+        yield current
+        current += delta
+
+
+def angular_distance(va, vb):
+    dlambda = np.fabs(vb[1].radians - va[1].radians)
+    phi1 = va[0].radians
+    phi2 = vb[0].radians
+
+    s1 = np.sin(phi1)
+    s2 = np.sin(phi2)
+
+    c1 = np.cos(phi1)
+    c2 = np.cos(phi2)
+
+    slam = np.sin(dlambda)
+    clam = np.cos(dlambda)
+
+    return np.arctan(np.sqrt((c2*slam)**2 + (c1*s2 - s1*c2*clam)**2) / (s1*s2 + c1*c2*clam))
+
+
+def load_stars(max_magnitude=5):
+    print("[  CATALOG] Loading star catalog data.")
+    with load.open(hipparcos.URL) as f:
+        df = hipparcos.load_dataframe(f)
+
+        df = df[df['magnitude'] <= max_magnitude]
+
+        print('[  CATALOG] Found {} stars brighter than magnitude {}.'.format(len(df),
+                                                                              max_magnitude))
+class Observation:
+    def __init__(self, time, site, obj, magnitude, radius):
+        self.t = time
+        self.site = site
+        self.obj = obj
+        # Compute right at the get-go
+        self.radius = radius
+        self.magnitude = magnitude
+
+    def alt_az(self):
+        return self.site.apparent_alt_az(self.t, self.obj)
+
+    def apparent_magnitude(self):
+        return self.magnitude
+
+    def apparent_radius(self):
+        return self.radius
+
+    def time(self):
+        return self.t
+
+    def summarize(self):
+        altaz = self.alt_az()
+        magnitude = self.apparent_magnitude()
+        radius = self.apparent_radius()
+
+        visibility = 'visible' if self.visible() else 'not visible'
+
+        return f"""{self.obj.name}, at {self.t} UTC:
+  altitude {altaz[0]}
+  azimuth  {altaz[1]}
+  magnitude {magnitude}
+  radius {radius} radians
+  {visibility} from {self.site}
+"""
+
+    def visible(self):
+        return self.alt_az()[0].radians > 0
+
+    def body(self):
+        return self.obj
+
+class Site:
+    def __init__(self, lat, lon, name):
+        self.topos = Topos(lat, lon)
+        self.lat = lat
+        self.lon = lon
+        self.ephemeris = load('de430.bsp')
+        self.location = self.ephemeris['earth'] + self.topos
+        self.ts = load.timescale()
+        self.name = name
+
+    def apparent_alt_az(self, t, obj):
+        alt, az, _ = self.location.at(self.ts.utc(t)).observe(obj.skyfield_obj).apparent().altaz()
+        return (alt, az)
+
+    def observe_solar_system(self, t, obj):
+        obj.ephem_obj.compute(t)
+        return Observation(t, self, obj, obj.ephem_obj.mag, obj.ephem_obj.radius)
+
+    def next_set_rise(self, day):
+        # By adding 2 days, we can be sure to find today's sunset and the following (tomorrow
+        # morning's) sunrise.
+        #
+        # TODO(MP): Like all other times, the UTC situation here is a bit crappy.  We should really
+        # just start searching at noon local time.
+        begin = self.ts.utc(day)
+        end = self.ts.utc(day + timedelta(days=2))
+        times, ups = almanac.find_discrete(begin, end,
+                                           almanac.sunrise_sunset(self.ephemeris, self.topos))
+        times = [t.astimezone(utc) for t in times]
+
+        # Find the first sunset
+        sunset = next(horizon for horizon in zip(times, ups) if not horizon[1])[0]
+        # Find the first sunrise after the first sunset
+        sunrise = next(horizon for horizon in zip(times, ups) if horizon[1] and horizon[0] > sunset)[0]
+        return (sunset, sunrise)
+
+    def __str__(self):
+        return f'{self.name}, at {self.lat}, {self.lon} (Earth)'
+
+    def __repr__(self):
+        return self.__str__()
+
+
+class SolarSystemBody():
+    def __init__(self, name, skyfield_obj, ephem_obj):
+        self.name = name
+        self.skyfield_obj = skyfield_obj
+        self.ephem_obj = ephem_obj
+
+    def __str__(self):
+        return f'{self.name}'
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __lt__(self, other):
+        return self.__str__() <= other.__str__()
+
+
+def load_solar_system():
+    print("[EPHEMERIS] Loading ephemeris data for planets / moon / Pluto.")
+    planet_barycenters = load('de430.bsp')
+
+    return [
+        SolarSystemBody('Mercury', planet_barycenters['mercury'], ephem.Mercury()),
+        SolarSystemBody('Venus', planet_barycenters['venus'], ephem.Venus()),
+        SolarSystemBody('Moon', planet_barycenters['moon'], ephem.Moon()),
+        SolarSystemBody('Mars', load('mar097.bsp')['mars'], ephem.Mars()),
+        SolarSystemBody('Jupiter', load('jup310.bsp')['jupiter'], ephem.Jupiter()),
+        SolarSystemBody('Saturn', load('sat319.bsp')['saturn'], ephem.Saturn()),
+        SolarSystemBody('Uranus', load('ura111.bsp')['uranus'], ephem.Uranus()),
+        SolarSystemBody('Neptune', load('nep081.bsp')['neptune'], ephem.Neptune()),
+        SolarSystemBody('Pluto', load('plu055.bsp')['pluto'], ephem.Pluto()),
+    ]
+
+
+class Star():
+    def __init__(self, name, skyfield_obj):
+        self.name = name
+        self.skyfield_obj = skyfield_obj
+
+    def __str__(self):
+        return f'{self.name}'
+
+    def __repr__(self):
+        return self.__str__()
+
+
+class Constraint():
+    def __init__(self, max_magnitude, max_distance):
+        self.max_magnitude = max_magnitude
+        self.max_distance = max_distance
+
+
+class Conjunction():
+    def __init__(self, first_obs, second_obs, constraint):
+        self.first_obs = first_obs
+        self.second_obs = second_obs
+        assert first_obs.time() == second_obs.time()
+        self.time = first_obs.time()
+        self.constraint = constraint
+
+    def valid(self):
+        if not self.first_obs.visible() or not self.second_obs.visible():
+            return False
+
+        maga, magb = self.first_obs.apparent_magnitude(), self.second_obs.apparent_magnitude()
+        if not maga <= self.constraint.max_magnitude or not magb <= self.constraint.max_magnitude:
+            return False
+
+        if np.abs(self.__distance()) > self.constraint.max_distance:
+            return False
+
+        return True
+
+
+    def __distance(self):
+        # In the unlikely event of overlapping objects, we'll get a negative distance.
+        return (angular_distance(self.first_obs.alt_az(),
+                                 self.second_obs.alt_az()) -
+                self.first_obs.apparent_radius() -
+                self.second_obs.apparent_radius())
+
+    def __magnitude_difference(self):
+        first_mag, second_mag = self.first_obs.apparent_magnitude(), self.second_obs.apparent_magnitude()
+        return np.abs(first_mag - second_mag)
+
+    def __max_magnitude(self):
+        first_mag, second_mag = self.first_obs.apparent_magnitude(), self.second_obs.apparent_magnitude()
+        return max(first_mag, second_mag)
+
+    def score(self):
+        distance = self.__distance()
+        magnitude_difference = self.__magnitude_difference()
+        max_magnitude = self.__max_magnitude()
+
+        return distance + magnitude_difference + max_magnitude
+
+    def __str__(self):
+        return f"""Conjunction between {self.first_obs.obj.name} and {self.second_obs.obj.name}:
+                time: {self.time}
+            distance: {self.__distance() * 180 / np.pi} degrees
+magnitude difference: {self.__magnitude_difference()} ({np.power(2.152, self.__magnitude_difference())} times brighter)
+   maximum magnitude: {self.__max_magnitude()}
+               score: {self.score()}
+"""
+
+    def __repr__(self):
+        return self.__str__()
+
+    def bodies(self):
+        # Return bodies in normalized order
+        s = sorted([self.first_obs.body(), self.second_obs.body()])
+        return (s[0], s[1])
+
+
+def sort_conjunctions_by_score(conjunctions):
+    return sorted(conjunctions, key=lambda x: x.score())
+
+
+def scan_night(site, day, constraint):
+    dt = timedelta(minutes=30)
+    sunset, sunrise = site.next_set_rise(day)
+
+    conjunctions = {}
+    for t in datetime_range(sunset, sunrise, dt):
+        # Form all the possible conjunction pairs
+        conj = [
+            Conjunction(site.observe_solar_system(t, p), site.observe_solar_system(t, q), constraint)
+            for p in solar_system
+            for q in solar_system
+            if p != q
+        ]
+
+        # Filter out invalid conjunctions
+        conj = [c for c in conj if c.valid()]
+        for c in conj:
+            # Save to the master dictionary, indexed by the bodies
+            if c.bodies() in conjunctions:
+                conjunctions[c.bodies()].append(c)
+            else:
+                conjunctions[c.bodies()] = [c]
+
+    outputs = []
+    for _, conjs in conjunctions.items():
+        # Find the best output for the whole night for this pair of bodies
+        outputs.append(sort_conjunctions_by_score(conjs)[0])
+
+    return sort_conjunctions_by_score(outputs)
+
+
+def scan_days(location, t0_, t1_, constraint=Constraint(5.0, 1 * np.pi / 180)):
+    t0 = t0_.replace(tzinfo=utc)
+    t1 = t1_.replace(tzinfo=utc)
+
+    dday = timedelta(days=1)
+
+    best_conjunctions = {}
+
+    for day in datetime_range(t0, t1, dday):
+        best_conjunctions[day] = scan_night(location, day, constraint)
+
+    return best_conjunctions
+
+def display_results(results, site, t0, t1, best=5):
+    print(f"""Displaying events in {site} between
+    {t0}
+ and
+    {t1}
+ (all times in UTC, for now):
+""")
+    for night, conjunctions in results.items():
+        num_conjunctions = len(conjunctions)
+        if num_conjunctions == 0:
+            continue
+        print(night, f'({num_conjunctions} event{"" if num_conjunctions == 1 else "s"})')
+        if num_conjunctions < best:
+            print(sort_conjunctions_by_score(conjunctions))
+        else:
+            print(sort_conjunctions_by_score(conjunctions)[0:best])
+
+if __name__ == '__main__':
+    solar_system = load_solar_system()
+
+    brisbane = Site('27.49665 S', '152.9883 E', "Brisbane")
+
+    t0 = datetime(2019, 9, 10).replace(tzinfo=utc)
+    t1 = datetime(2019, 9, 25).replace(tzinfo=utc)
+
+    results = scan_days(brisbane, t0, t1)
+    display_results(results, brisbane, t0, t1)
+
+    load_stars()
+    print("Star conjunction support coming soon!")
