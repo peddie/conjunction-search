d60a484a3a1757e05a7084cbdba1451d8624d32a
Matthew Peddie <mpeddie@gmail.com>
2019-11-20 22:30:26 +1000

    A bunch of updates
     - Star conjunctions are now supported
     - Time zone handling is sane
     - Various output formatting tweaks
     - Nifty progress bars

--- search.py
+++ search.py
@@ -5,6 +5,11 @@ from skyfield.api import Star, load, Topos, utc
 from skyfield.data import hipparcos
 from skyfield.units import Angle
 from skyfield import almanac
+import pytz
+from tzwhere import tzwhere
+import re
+import time
+from tqdm import tqdm
 
 
 def datetime_range(start, end, delta):
@@ -32,14 +37,21 @@ def angular_distance(va, vb):
 
 
 def load_stars(max_magnitude=5):
+    """Load the Hipparcos catalog data and find all applicable stars.  Returns a
+    list of StarObs, one per star.
+    """
     print("[  CATALOG] Loading star catalog data.")
     with load.open(hipparcos.URL) as f:
         df = hipparcos.load_dataframe(f)
 
         df = df[df['magnitude'] <= max_magnitude]
+        df = df[df['ra_degrees'].notnull()]
 
-        print('[  CATALOG] Found {} stars brighter than magnitude {}.'.format(len(df),
-                                                                              max_magnitude))
+        print(f'[  CATALOG] Found {len(df)} stars brighter than magnitude {max_magnitude:.4f}.')
+        return [StarObs(str(idx), Star.from_dataframe(df.loc[idx]), df.loc[idx].magnitude)
+                for idx in df.index]
+
+
 class Observation:
     def __init__(self, time, site, obj, magnitude, radius):
         self.t = time
@@ -68,7 +80,7 @@ class Observation:
 
         visibility = 'visible' if self.visible() else 'not visible'
 
-        return f"""{self.obj.name}, at {self.t} UTC:
+        return f"""{self.obj.name}, at {self.t} {self.site.timezone}:
   altitude {altaz[0]}
   azimuth  {altaz[1]}
   magnitude {magnitude}
@@ -82,6 +94,7 @@ class Observation:
     def body(self):
         return self.obj
 
+
 class Site:
     def __init__(self, lat, lon, name):
         self.topos = Topos(lat, lon)
@@ -91,14 +104,20 @@ class Site:
         self.location = self.ephemeris['earth'] + self.topos
         self.ts = load.timescale()
         self.name = name
+        lat_value, lon_value = ll_string_to_float(lat), ll_string_to_float(lon)
+        self.timezone = pytz.timezone(tzwhere.tzwhere().tzNameAt(lat_value, lon_value))
+        print(f"Time zone in {name}: {self.timezone}")
+
+    def localize(self, some_time):
+        return self.timezone.localize(some_time)
 
     def apparent_alt_az(self, t, obj):
         alt, az, _ = self.location.at(self.ts.utc(t)).observe(obj.skyfield_obj).apparent().altaz()
         return (alt, az)
 
-    def observe_solar_system(self, t, obj):
-        obj.ephem_obj.compute(t)
-        return Observation(t, self, obj, obj.ephem_obj.mag, obj.ephem_obj.radius)
+    def observe(self, t, obj):
+        obj.update_state(t)
+        return Observation(t, self, obj, obj.get_magnitude(), obj.get_radius())
 
     def next_set_rise(self, day):
         # By adding 2 days, we can be sure to find today's sunset and the following (tomorrow
@@ -110,7 +129,7 @@ class Site:
         end = self.ts.utc(day + timedelta(days=2))
         times, ups = almanac.find_discrete(begin, end,
                                            almanac.sunrise_sunset(self.ephemeris, self.topos))
-        times = [t.astimezone(utc) for t in times]
+        times = [t.astimezone(self.timezone) for t in times]
 
         # Find the first sunset
         sunset = next(horizon for horizon in zip(times, ups) if not horizon[1])[0]
@@ -132,13 +151,23 @@ class SolarSystemBody():
         self.ephem_obj = ephem_obj
 
     def __str__(self):
-        return f'{self.name}'
+        return f'SolarSystemBody({self.name})'
 
     def __repr__(self):
         return self.__str__()
 
     def __lt__(self, other):
         return self.__str__() <= other.__str__()
+
+    def update_state(self, t):
+        self.ephem_obj.compute(t)
+        return None
+
+    def get_magnitude(self):
+        return self.ephem_obj.mag
+
+    def get_radius(self):
+        return self.ephem_obj.radius
 
 
 def load_solar_system():
@@ -158,22 +187,33 @@ def load_solar_system():
     ]
 
 
-class Star():
-    def __init__(self, name, skyfield_obj):
+class StarObs():
+    def __init__(self, name, skyfield_obj, magnitude):
         self.name = name
         self.skyfield_obj = skyfield_obj
+        self.magnitude = magnitude
 
     def __str__(self):
-        return f'{self.name}'
+        return f'StarObs({self.name}, mag {self.magnitude})'
 
     def __repr__(self):
         return self.__str__()
 
+    def update_state(self, t):
+        return None
+
+    def get_magnitude(self):
+        return self.magnitude
+
+    def get_radius(self):
+        return 0.0
+
 
 class Constraint():
-    def __init__(self, max_magnitude, max_distance):
+    def __init__(self, max_magnitude, max_distance, max_magnitude_delta):
         self.max_magnitude = max_magnitude
         self.max_distance = max_distance
+        self.max_magnitude_delta = max_magnitude_delta
 
 
 class Conjunction():
@@ -190,6 +230,9 @@ class Conjunction():
 
         maga, magb = self.first_obs.apparent_magnitude(), self.second_obs.apparent_magnitude()
         if not maga <= self.constraint.max_magnitude or not magb <= self.constraint.max_magnitude:
+            return False
+
+        if np.abs(maga - magb) > self.constraint.max_magnitude_delta:
             return False
 
         if np.abs(self.__distance()) > self.constraint.max_distance:
@@ -223,10 +266,10 @@ class Conjunction():
     def __str__(self):
         return f"""Conjunction between {self.first_obs.obj.name} and {self.second_obs.obj.name}:
                 time: {self.time}
-            distance: {self.__distance() * 180 / np.pi} degrees
-magnitude difference: {self.__magnitude_difference()} ({np.power(2.152, self.__magnitude_difference())} times brighter)
-   maximum magnitude: {self.__max_magnitude()}
-               score: {self.score()}
+            distance: {self.__distance() * 180 / np.pi:.3f} degrees
+magnitude difference: {self.__magnitude_difference():.3f} ({np.power(2.152, self.__magnitude_difference()):.3f} times brighter)
+   maximum magnitude: {self.__max_magnitude():.3f}
+               score: {self.score():.5f}
 """
 
     def __repr__(self):
@@ -243,21 +286,29 @@ def sort_conjunctions_by_score(conjunctions):
 
 
 def scan_night(site, day, constraint):
+    t0 = time.perf_counter()
     dt = timedelta(minutes=30)
     sunset, sunrise = site.next_set_rise(day)
 
     conjunctions = {}
-    for t in datetime_range(sunset, sunrise, dt):
+    for t in tqdm(datetime_range(sunset, sunrise, dt),
+                  desc='Time windows', unit='win', leave=None,
+                  total=len(list(datetime_range(sunset, sunrise, dt)))):
         # Form all the possible conjunction pairs
         conj = [
-            Conjunction(site.observe_solar_system(t, p), site.observe_solar_system(t, q), constraint)
-            for p in solar_system
-            for q in solar_system
-            if p != q
+            c for c in tqdm((Conjunction(site.observe(t, p), site.observe(t, q), constraint)
+                             # Scan only solar system objects for the first index, since stars
+                             # are obviously static
+                             for p in solar_system
+                             for q in sky_objects
+                             if p != q),
+                            desc='Conjunctions scanned', unit='conj', leave=None)
+            if c.valid()
+
         ]
 
         # Filter out invalid conjunctions
-        conj = [c for c in conj if c.valid()]
+        # conj = [c for c in conj if c.valid()]
         for c in conj:
             # Save to the master dictionary, indexed by the bodies
             if c.bodies() in conjunctions:
@@ -270,28 +321,41 @@ def scan_night(site, day, constraint):
         # Find the best output for the whole night for this pair of bodies
         outputs.append(sort_conjunctions_by_score(conjs)[0])
 
-    return sort_conjunctions_by_score(outputs)
+    t1 = time.perf_counter()
+    return sort_conjunctions_by_score(outputs), t1 - t0
 
 
-def scan_days(location, t0_, t1_, constraint=Constraint(5.0, 1 * np.pi / 180)):
-    t0 = t0_.replace(tzinfo=utc)
-    t1 = t1_.replace(tzinfo=utc)
+def scan_days(location, t0_, t1_, constraint=Constraint(5.0, 1 * np.pi / 180, 3.0)):
+    t0 = location.localize(t0_)
+    t1 = location.localize(t1_)
 
     dday = timedelta(days=1)
 
     best_conjunctions = {}
 
-    for day in datetime_range(t0, t1, dday):
-        best_conjunctions[day] = scan_night(location, day, constraint)
+    print(f"[   SEARCH] Scanning nights from {t0} to {t1}")
+    print(f"[   SEARCH] This will take a while!")
+    calc_times = []
+    for day in tqdm(datetime_range(t0, t1, dday),
+                    desc='Nights', unit='n', leave=None,
+                    total=len(list(datetime_range(t0, t1, dday)))):
+        best_conjunctions[day], calc_time = scan_night(location, day, constraint)
+        calc_times.append(calc_time)
+
+    mean_time_per_night = np.mean(calc_times)
+    total_time = np.sum(calc_times)
+    print(f"[   SEARCH] Scanning took {total_time:.3f}s for {len(calc_times)} nights.")
+    print(f"[   SEARCH] Mean scan time was {mean_time_per_night:.3f}s per night.")
 
     return best_conjunctions
+
 
 def display_results(results, site, t0, t1, best=5):
     print(f"""Displaying events in {site} between
     {t0}
  and
     {t1}
- (all times in UTC, for now):
+ :
 """)
     for night, conjunctions in results.items():
         num_conjunctions = len(conjunctions)
@@ -303,16 +367,47 @@ def display_results(results, site, t0, t1, best=5):
         else:
             print(sort_conjunctions_by_score(conjunctions)[0:best])
 
+
+def ll_string_to_float(string):
+    cardinal = ''.join(c for c in string if c in "nsewNSEW")
+    magnitude = ''.join(c for c in string if c.isdigit() or c == '.')
+    sign = 1
+    if cardinal in "sSwW":
+        sign = -1
+
+    return sign * float(magnitude)
+
+
+def float_to_lat_string(value):
+    cardinal = 'N'
+    if value < 0:
+        cardinal = 'S'
+
+    magnitude = str(abs(value))
+    return f"{magnitude} {cardinal}"
+
+
+def float_to_lon_string(value):
+    cardinal = 'E'
+    if value < 0:
+        cardinal = 'W'
+
+    magnitude = str(abs(value))
+    return f"{magnitude} {cardinal}"
+
+
 if __name__ == '__main__':
     solar_system = load_solar_system()
+    stars = load_stars(max_magnitude=3)
 
-    brisbane = Site('27.49665 S', '152.9883 E', "Brisbane")
+    sky_objects = solar_system + stars
 
-    t0 = datetime(2019, 9, 10).replace(tzinfo=utc)
-    t1 = datetime(2019, 9, 25).replace(tzinfo=utc)
+    brisbane_lat_string, brisbane_lon_string = '27.49665 S', '152.9883 E'
+
+    brisbane = Site(brisbane_lat_string, brisbane_lon_string, "Brisbane")
+
+    t0 = datetime(2019, 9, 11)
+    t1 = datetime(2019, 9, 12)
 
     results = scan_days(brisbane, t0, t1)
     display_results(results, brisbane, t0, t1)
-
-    load_stars()
-    print("Star conjunction support coming soon!")
